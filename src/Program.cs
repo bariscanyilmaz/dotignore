663f07c0c0df122336150981e3312d44c5b6c490
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-24 23:44:52 +0300

    add depended services

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Options;
+using Services;
 using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace dotignore
 {
@@ -7,19 +9,19 @@ namespace dotignore
     {
         static void Main(string[] args)
         {
+            var services = new ServiceCollection();
+            services.AddHttpClient<WebService>();
+            services.AddTransient<FileService>();
+            services.AddTransient<TemplateService>();
+            var serviceProvider = services.BuildServiceProvider();
+
+            var templateService = serviceProvider.GetService<TemplateService>();
+
             Parser.Default.ParseArguments<InitOption, ListOption>(args)
-            .WithParsed<InitOption>(option=>RunInitOption(option))
-            .WithParsed<ListOption>(option=>RunListOption(option))
-            .WithNotParsed(errors=>errors.Output());
+            .WithParsedAsync<InitOption>(async options => await templateService.InitializeTemplateAsync(options)).GetAwaiter().GetResult()
+            .WithParsed<ListOption>(options => templateService.ListTemplates(options))
+            .WithNotParsed(errors => errors.Output());
         }
 
-        static void RunInitOption(InitOption option)
-        {
-
-        }
-        static void RunListOption(ListOption option)
-        {
-
-        }
     }
 }
