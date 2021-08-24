d7402d7b3d471cd5313e3e495f704716b943e239
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-24 11:09:20 +0300

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotignore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
