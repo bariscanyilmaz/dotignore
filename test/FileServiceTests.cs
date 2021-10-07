98e8a272ff56e38f13052035bdfccb1592701f5f
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-10-07 17:25:02 +0300

    add testing helper

--- test/FileServiceTests.cs
+++ test/FileServiceTests.cs
@@ -1,10 +1,11 @@
-
 using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using System.Threading;
 using System.Threading.Tasks;
 using Moq;
 using Services;
 using Xunit;
+
 
 namespace dotignore.test
 {
@@ -14,19 +15,32 @@ namespace dotignore.test
 
         public FileServiceTests()
         {
-            _fileSystem=new Mock<IFileSystem>();
+            _fileSystem = new Mock<IFileSystem>();
         }
 
         [Fact]
         public async Task CreateIgnoreFileAsync_ShouldCreate_WhenCalled()
         {
-            _fileSystem.Setup(x=>x.File.WriteAllTextAsync(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+            _fileSystem.Setup(x => x.File.WriteAllTextAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
 
-            var fileService=new FileService(_fileSystem.Object);
-            string content="content";
+            var fileService = new FileService(_fileSystem.Object);
+            string content = "content";
             await fileService.CreateIgnoreFileAsync(content);
 
-            _fileSystem.Verify(x=>x.File.WriteAllTextAsync(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<CancellationToken>()));
+            _fileSystem.Verify(x => x.File.WriteAllTextAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()));
         }
+
+        [Fact]
+        public async Task AppendIgnoreFileAsync_ShouldAppend_WhenCalled()
+        {
+            _fileSystem.Setup(x=>x.File.AppendAllTextAsync(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+
+            var fileService = new FileService(_fileSystem.Object);
+            string content = "content";
+            await fileService.AppendIgnoreFileAsync(content);
+
+            _fileSystem.Verify(x=>x.File.AppendAllTextAsync(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<CancellationToken>()));
+        }
+
     }
 }
